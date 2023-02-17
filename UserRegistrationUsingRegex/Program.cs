961b6c7598b0fe91eec8955556c7a55a6328aa4f
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-17 22:31:13 +0530

    [Sai Venkat] Added- Validating First Name using Regular Expression

---
+++ UserRegistrationUsingRegex/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Using Regular Expression");
+            ValidatingUser.FName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistrationUsingRegex/ValidatingUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingRegex
+{
+    internal class ValidatingUser
+    {
+        public static void FName()
+        {
+            //"^[A-Z]{1}[a-z]{2,}$"
+            string pattren = "^[A-Z]{1}[a-z]{2,}$";
+            Console.WriteLine("Enter the First Name :");
+            string fName=Console.ReadLine();
+            if (Regex.IsMatch(fName, pattren))
+            {
+                Console.WriteLine("Name is Valid");
+            }
+            else
+                Console.WriteLine("Name is InValid");
+        }
+    }
+}
